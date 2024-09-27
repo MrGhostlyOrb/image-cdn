f6fd8fe42734a3f6875b4a75f668673a1e0f60e7
Ben Cooper <ben.f.cooper@gmail.com>
2024-09-27 15:28:35 +0100

    ⚡️ improve performance and refactor

--- main.go
+++ main.go
@@ -12,33 +12,42 @@ import (
 
 func main() {
 	year, month, day := time.Now().Date()
-	date := fmt.Sprint("[CDN startup] The date is ", day, " ", month, " ", year)
+	date := fmt.Sprintf("[CDN startup] The date is %d %s %d", day, month, year)
 	log.Output(1, date)
 	http.HandleFunc("/coke_promotion_banner.webp", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/webp")
-		file := prepareFile(month)
+		file, err := prepareFile(month)
+		if err != nil {
+			log.Output(1, fmt.Sprint("[CDN] ", err))
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 		w.Write(file)
 	})
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
-func prepareFile(month time.Month) []byte {
+func prepareFile(month time.Month) ([]byte, error) {
 	var buf []byte
-	entries, err := os.ReadDir(fmt.Sprint(strings.ToLower(month.String())))
+	monthLower := strings.ToLower(month.String())
+	entries, err := os.ReadDir(fmt.Sprint(monthLower))
 	if err != nil || len(entries) < 1 {
 		buf, err = os.ReadFile("default.webp")
 		if err != nil {
-			log.Output(1, "[CDN] no default file found")
+			return nil, err
 		} else {
 			log.Output(1, "[CDN] serving default.webp")
 		}
 	} else {
-		buf, err = os.ReadFile(fmt.Sprint(strings.ToLower(month.String()), "/", entries[rand.Intn(len(entries))].Name()))
+		randomImageIndex := rand.Intn(len(entries))
+		buf, err = os.ReadFile(fmt.Sprint(monthLower, "/", entries[randomImageIndex].Name()))
 		if err != nil {
-			log.Output(1, fmt.Sprint("[CDN] serving ", strings.ToLower(month.String()), "/", entries[rand.Intn(len(entries))].Name()))
+			return nil, err
+		} else {
+			log.Output(1, fmt.Sprint("[CDN] serving ", monthLower, "/", entries[randomImageIndex].Name()))
 		}
 	}
 
-	return buf
+	return buf, nil
 }
